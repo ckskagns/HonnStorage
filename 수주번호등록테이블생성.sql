9c2775fb06d29848c6f38e7f3a9fd7ecce84e312
ckskagns <62416175+ckskagns@users.noreply.github.com>
2020-07-17 15:02:47 +0900

    Update 수주번호등록테이블생성.sql

--- 수주번호등록테이블생성.sql
+++ 수주번호등록테이블생성.sql
@@ -2,23 +2,31 @@ select * from HAA010T
 
 begin tran
 CREATE TABLE Ex_Number (
-       EMP_NO                nvarchar(26)             NOT NULL,
+       YYYY        nchar(4)             NOT NULL,
+       EMP_NO      nvarchar(26)             NOT NULL,
        ST_NUM      nvarchar(40),
        BIND_NUM    nvarchar(40),  
-       EMAIL       nvarchar(80)
+       EMAIL       nvarchar(80),
+	   ETC_QTY1    nvarchar(40),
+	   ETC_QTY2    nvarchar(40),
+	   ETC_QTY3    nvarchar(40),
+	   ISRT_EMP_NO nvarchar(40),
+	   ISRT_DT     datetime,
+	   UPDT_EMP_NO nvarchar(40),
+	   UPDT_DT     datetime
     
-      CONSTRAINT PK_Ex_NUM PRIMARY KEY (EMP_NO)
+      CONSTRAINT PK_Ex_NUM PRIMARY KEY (EMP_NO,YYYY)
     
 );
 
 commit
 
 begin tran
-INSERT INTO Ex_Number (EMP_NO) SELECT EMP_NO FROM HAA010T
+INSERT INTO Ex_Number (YYYY,EMP_NO) SELECT YYYY,DILIG_EMP_NO FROM H4006M3_Q_KO883
 
 commit
 
-select * from Ex_Number
+select * from Ex_Number order by yyyy
 
 
 
