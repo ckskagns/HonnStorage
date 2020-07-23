703b6b51856449ba7b76cc81466c38d6d1fc9e25
ckskagns <62416175+ckskagns@users.noreply.github.com>
2020-07-23 14:51:33 +0900

    Update 수주번호등록조회SP.sql

--- 수주번호등록조회SP.sql
+++ 수주번호등록조회SP.sql
@@ -1,9 +1,10 @@
 
-ALTER PROCEDURE [dbo].[USP_H4007M5_KO883P_111]            
+CREATE PROCEDURE [dbo].[USP_H4007M5_KO883P_111]            
  (                        
 	@EMP_NO    nvarchar(26)  --사번                
     , @DEPT_CD nvarchar(26)  --부서  
-	, @YYYY    nchar(4)      --기준년도                     
+	, @YYYY    nchar(4)      --기준년도
+	, @EMAIL   nvarchar(40)  --이메일                     
  ) AS                      
                 
  BEGIN                                          
@@ -27,8 +28,9 @@ select
  from HAA010T(NOLOCK) a    
  join Ex_Number B on A.emp_no = B.emp_no
  join dbo.ufn_h_getdeptdata(GETDATE()) dept on A.EMP_NO = dept.emp_no
- where B.YYYY = @YYYY and (@EMP_NO ='' or A.EMP_NO = @EMP_NO ) and (@DEPT_CD = '' or dept.dept_cd = @DEPT_CD)     
+ where B.YYYY = @YYYY and (@EMP_NO ='' or A.EMP_NO = @EMP_NO ) and (@DEPT_CD = '' or dept.dept_cd = @DEPT_CD)  and (@EMAIL = '' or B.EMAIL LIKE '%' +@EMAIL + '%')     
   order by   a.ROLL_PSTN    
             
  END 
 
+ --exec USP_H4007M5_KO883P_111 @YYYY=N'2020',@EMP_NO=N'',@DEPT_CD=N'',@EMAIL=''
